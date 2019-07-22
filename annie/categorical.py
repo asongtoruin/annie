c440297cc488cd3476600451a3468b0334c0faaf
Adam <adam.ruszkowski@gmail.com>
2019-07-22 22:06:45 +0100

    Rework bar plot
    Horizontal plots weren't working out because it was written badly. By
    renaming the variables and rejigging the calculations somewhat, it works
    and is a little easier to understand. Could probably still be written a
    lot better, mind.

--- annie/categorical.py
+++ annie/categorical.py
@@ -7,31 +7,47 @@ from . import AnimatedGraph, copy_dict
 
 
 class AnimatedBar(AnimatedGraph):
-    def __init__(self, x, y, y_lims=None,
+    def __init__(self, categories, values, orient='v', value_lims=None,
                  plot_kwargs=None, legend_kwargs=None):
-        orient = plot_kwargs.get('orient', 'v') if plot_kwargs else 'v'
+        temp_p_kws = copy_dict(plot_kwargs) if plot_kwargs else dict()
 
         if orient == 'v':
             animate_from = 'x'
-        else:
+            x_vals = categories
+            y_vals = values
+            self._value_ax = 'y'
+        elif orient == 'h':
             animate_from = 'y'
+            x_vals = values
+            y_vals = categories
+            self._value_ax = 'x'
+        else:
+            raise ValueError
+
+        temp_p_kws['orient'] = orient
 
         super().__init__(
-            animate_from=animate_from, x=x, y=y, func=sns.barplot,
+            animate_from=animate_from, x=x_vals, y=y_vals, func=sns.barplot,
             plot_kwargs=plot_kwargs, legend_kwargs=legend_kwargs
         )
 
         # Do an initial plot to get the axis limits for our final frame.
-        if not y_lims:
+        if not value_lims:
             ax = sns.barplot(
-                self._x_plot, self._y_plot, legend=False, **self.plot_kwargs
+                self._x_plot, self._y_plot, **self.plot_kwargs
             )
-            if not y_lims:
-                y_lims = ax.get_ylim()
+            print('finished initial plot')
+            if self._value_ax == 'x':
+                value_lims = ax.get_xlim()
+            else:
+                value_lims = ax.get_ylim()
 
-        self._y_lims = y_lims
+        self._value_lims = value_lims
         plt.close()
 
     def _animate(self, i):
         super()._animate(i)
-        self.ax.set_ylim(*self._y_lims)
+        if self._value_ax == 'x':
+            self.ax.set_xlim(*self._value_lims)
+        else:
+            self.ax.set_ylim(*self._value_lims)
